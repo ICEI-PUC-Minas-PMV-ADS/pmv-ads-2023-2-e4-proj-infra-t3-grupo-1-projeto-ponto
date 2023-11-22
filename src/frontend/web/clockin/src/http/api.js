36e120e01f332b41996ea7349df22e288228a115
Matheus Estevam <68551343+matheusenc@users.noreply.github.com>
2023-11-22 17:59:36 -0300

    azure-connection
    Conexão do front com o backend ja hospedado na azure

--- src/frontend/web/clockin/src/http/api.js
+++ src/frontend/web/clockin/src/http/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://10.0.0.101:8000",
+  baseURL: "http://clockin.bmddhadwg0esdeeb.brazilsouth.azurecontainer.io:80",
 });
 
 export default api;
